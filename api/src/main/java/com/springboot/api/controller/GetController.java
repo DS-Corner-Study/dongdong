90688101a8457d3431164e041728d2ef8014e47e
박유정 <p_1321@duksung.ac.kr>
2023-10-28 23:33:23 +0900

    5주차 [스프링 부트 핵심 가이드] 5장 로깅 라이브러리 - Logback

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -3,21 +3,31 @@ package com.springboot.api.controller;
 import com.springboot.api.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET) // http://localhost:8080/api/v1/get-api/hello
     public String getHello() {
+        LOGGER.info("getHello 메서드 호출됨.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name") // http://localhost:8080/api/v1/get-api/name
     public String getName(){
+        LOGGER.info("getName 메서드 호출됨.");
         return "Flature";
     }
 
@@ -29,6 +39,7 @@ public class GetController {
     @GetMapping(value = "/variable1/{variable}") // http://localhost:8080/api/v1/get-api/variable1/{String 값}
 //    public String getVariable(@PathVariable("variable") String var){
     public String getVariable(@PathVariable(value = "variable") String var){
+        LOGGER.info("@PathVariable을 통해 들어온 값: {}", var);
         return var;
     }
 
