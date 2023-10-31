a3f96e3cd5f8bcecf6da04836a3697b423caf43e
박유정 <p_1321@duksung.ac.kr>
2023-11-01 01:50:01 +0900

    5주차 [스프링 부트 핵심 가이드] 5장 퀴즈 추가 작성

--- api/src/main/java/com/springboot/api/dto/MemberDto.java
+++ api/src/main/java/com/springboot/api/dto/MemberDto.java
@@ -1,9 +1,25 @@
 package com.springboot.api.dto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(value = "회원 정보", description = "이름, 이메일, 회사, 나이를 가진 Domain Class")
 public class MemberDto {
+    @ApiModelProperty(value = "이름")
     private String name;
+    @ApiModelProperty(value = "이메일")
     private String email;
+    @ApiModelProperty(value = "회사")
     private String organization;
+    @ApiModelProperty(value = "나이")
+    private Number age;
+
+    public Number getAge() {
+        return age;
+    }
+
+    public void setAge(Number age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
@@ -31,6 +47,6 @@ public class MemberDto {
 
     @Override
     public String toString(){
-        return "MemberDto{"+"name='"+name+'\''+", email='"+email+'\''+", organization='"+organization+'\''+'}';
+        return "MemberDto{"+"name='"+name+'\''+", email='"+email+'\''+", organization='"+organization+'\''+", age='"+age+'\''+'}';
     }
 }
